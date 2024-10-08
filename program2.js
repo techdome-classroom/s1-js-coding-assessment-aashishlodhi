6ea0028738d1a16d4ae368a8f6f48de7806c567c
Aashish Lodhi <111765947+aashishlodhi@users.noreply.github.com>
2024-10-08 13:15:41 +0530

    solution program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,39 @@
 const decodeTheRing = function (s, p) {
+    let m = 0;
+    let pIndex = 0;
+    let starIndex = -1;
+    let matchIndex = -1;
 
-    // write your code here
+    while (m < s.length) {
 
-  };
-  
-  module.exports = decodeTheRing;
+        if (pIndex < p.length && (p[pIndex] === s[m] || p[pIndex] === '?')) {
+            m++;
+            pIndex++;
+        }
+
+        else if (pIndex < p.length && p[pIndex] === '*') {
+            starIndex = pIndex;
+            matchIndex = m;
+            pIndex++;
+        }
+
+        else if (starIndex !== -1) {
+            pIndex = starIndex + 1;
+            matchIndex++;
+            m = matchIndex;
+        }
+
+        else {
+            return false;
+        }
+    }
+
+    while (pIndex < p.length && p[pIndex] === '*') {
+        pIndex++;
+    }
+
+
+    return pIndex === p.length;
+};
+
+module.exports = decodeTheRing;
